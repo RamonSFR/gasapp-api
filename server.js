db95fe8fc3287248b89277b4bb24bfc43c4c8245
RamonSFR <ramonsaviofontesrocha@gmail.com>
2025-03-29 14:51:07 -0300

    receving atributes via HTTP

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
 app.post('/calc', (req, res) => {
-    res.send('Hello World')
+    const { ethConsume, gasConsume, ethPrice, gasPrice, fuelingValue } = req.body
+    
+    if (!ethConsume || !gasConsume || !ethPrice || !gasPrice || !fuelingValue) {
+        res.status(400).json({"error" : "all atributes are necessary"})
+    }
+
+    res.send(`${ethConsume} ${gasConsume} ${ethPrice} ${gasPrice} ${fuelingValue}`)
 })
 
-const PORT = '3000'
+const PORT = process.env.PORT || 3000;
 app.listen(3000, () => console.log(`server running on port ${PORT}`))
